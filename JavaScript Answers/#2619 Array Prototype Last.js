81520fa6a41d7ea80d5ebacd07be883cb7c0b1d9
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2024-02-21 22:44:47 +0530

    day 20

---
+++ JavaScript Answers/#2619 Array Prototype Last.js
@@ -0,0 +1,11 @@
+// Enhance the Array prototype to include the last() method
+Array.prototype.last = function() {
+    return this.length > 0 ? this[this.length - 1] : -1;
+};
+
+// Example usage:
+const nums1 = [null, {}, 3];
+console.log(nums1.last()); // Output: 3
+
+const nums2 = [];
+console.log(nums2.last()); // Output: -1
